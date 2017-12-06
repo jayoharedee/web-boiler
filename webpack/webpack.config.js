8c5371684e48adc8dcf2c6b5a45dda596eecc33f
Jordan Michael <jordanm@tuta.io>
2017-12-05 23:39:56 -0500

    WIP: webpack stuff is really antiquated and needs some TLC

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   entry: '../index.js',
   output: {
-      filename: '../src/bundle.js'
+    filename: '../src/bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 }
